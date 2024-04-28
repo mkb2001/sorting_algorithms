5368ece6aaf970367dfbf1ca9a2224a992691a02
mkb2001 <kevinmugarura051@gmail.com>
2024-04-28 23:11:28 +0300

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,21 +6,32 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i;
-    size_t j;
-    for (i = 0; i < size - 1; i++) {
+    size_t i, len = size;
+    bool bubbly = false;
+
+    if (array == NULL || size < 2)
+        return;
+
+    while (bubbly == false)
+    {
+        bubbly = true;
+        for (i = 0; i < len - 1; i++)
         {
-            for (j = 0; j < size - i - 1; j++) {
-                {
-                    if (array[j] < array[j + 1]) {
-                        swap(&array[j], &array[j + 1]);
-                        print_array(array, size);
-                    }
-                }
+            if (array[i] > array[i + 1])
+            {
+                swap(array + i, array + i + 1);
+                print_array(array, size);
+                bubbly = false;
             }
         }
-    }
-}
+        len--;
+}}
+
+/**
+ * swap - Swapps the values of two numbers
+ * @a: First Number
+ * @b: Second Number
+ */
 void swap(int* a, int* b)
 {
     int temp = *a;
